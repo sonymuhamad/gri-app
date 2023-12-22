3916f3546bcda02fe1d112980f32e301cc0b1ee0
sonymuhamad <sonyfadhil11@gmail.com>
2023-12-22 16:43:49 +0700

    fix-build-stg

--- src/app/admin/user/page.tsx
+++ src/app/admin/user/page.tsx
@@ -1,7 +1,5 @@
 import prisma from "@/db/db";
 
-import jwt from "jsonwebtoken";
-
 import TambahUser from "./_components/add-user";
 import TableUser from "./_components/table-user";
 
@@ -11,12 +9,6 @@ export default async function UserPage() {
       proyek: true,
     },
   });
-  console.log(process.env.SECRET_KEY);
-  const token = jwt.sign(
-    { ok: true, name: "Sony" },
-    process.env.SECRET_KEY as string
-  );
-  const verify = jwt.verify(token, process.env.SECRET_KEY as string);
 
   return (
     <div className="flex flex-col space-y-6">
