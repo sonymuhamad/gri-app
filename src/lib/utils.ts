28b26cb4ffe85beacb54278aa34bc5f77a299de4
sonymuhamad <sonyfadhil11@gmail.com>
2024-01-08 12:37:30 +0700

    feat format date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -37,3 +37,8 @@ export function getTodayAndTomorrow() {
 
   return [today, tomorrow];
 }
+
+export function isValidDate(dateString: string) {
+  const dateRegex = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  return dateRegex.test(dateString);
+}
